372de2dcf2a7edc6a6b77ef286723682661f4c1f
aboitreaud <72934368+aboitreaud@users.noreply.github.com>
2023-11-30 11:18:52 +0100

    remove Head class

--- model.py
+++ model.py
@@ -34,35 +34,6 @@ class LayerNorm(nn.Module):
         return nn.functional.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
 
 
-class Head(nn.Module):
-    """ one head of self-attention """
-
-    def __init__(self, config):
-        super().__init__()
-        self.key = nn.Linear(config.embed_dim_total, config.n_embd, bias=False)
-        self.query = nn.Linear(config.embed_dim_total, config.n_embd, bias=False)
-        self.value = nn.Linear(config.embed_dim_total, config.n_embd, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(config.block_size, config.block_size)))
-
-        self.dropout = nn.Dropout(config.dropout)
-
-    def forward(self, x):
-        # input of size (batch, time-step, channels)
-        # output of size (batch, time-step, head size)
-        B,T,C = x.shape
-        k = self.key(x)   # (B,T,hs)
-        q = self.query(x) # (B,T,hs)
-        # compute attention scores ("affinities")
-        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5  # (B, T, hs) @ (B, hs, T) -> (B, T, T)
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))  # (B, T, T)
-        wei = nn.functional.softmax(wei, dim=-1)  # (B, T, T)
-        wei = self.dropout(wei)
-        # perform the weighted aggregation of the values
-        v = self.value(x)  # (B,T,hs)
-        out = wei @ v  # (B, T, T) @ (B, T, hs) -> (B, T, hs)
-        return out
-
-
 class MultiHeadAttention(nn.Module):
     """ multiple heads of self-attention in parallel """
 
@@ -83,7 +54,6 @@ class MultiHeadAttention(nn.Module):
         # out = self.dropout(self.proj(out))
         out = self.proj(attn_output)
         return out
-
 
 
 class MLP(nn.Module):
