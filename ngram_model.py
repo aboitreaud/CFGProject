a9aea9a4e406bbabf9b27404c275917b242f5149
aboitreaud <aboitreaud@gmail.com>
2024-02-13 23:16:32 +0800

    Fix smooth prob in gen next token

--- ngram_model.py
+++ ngram_model.py
@@ -5,8 +5,9 @@ from context_free_grammar import CFG
 
 
 class NGramModel:
-    def __init__(self, n, cfg: CFG):
+    def __init__(self, n, smoothing_factor, cfg: CFG):
         self.n = n
+        self.smoothing_factor = smoothing_factor
         self.ngrams = defaultdict(lambda: defaultdict(int))
         self.cfg = cfg
 
@@ -21,16 +22,13 @@ class NGramModel:
             self.ngrams[context][next_token] += 1
 
     def predict_next_token(self, context):
+        assert len(context) == self.n - 1
         # Given a context, predict the next token
         context = tuple(context.tolist())
         next_tokens = list(self.ngrams[context].keys())
-        probabilities = [self.ngrams[context][token] for token in next_tokens]
-        if np.sum(probabilities) == 0:
-            print("Context not found in dict", context)
-            next_token = np.random.randint(self.cfg.ns[-1])
-        else:
-            probabilities = np.array(probabilities) / np.sum(probabilities)
-            next_token = np.random.choice(next_tokens, p=probabilities)
+        probabilities = [self._calculate_smoothed_prob(context, token) for token in next_tokens]
+        probabilities = np.array(probabilities) / np.sum(probabilities)
+        next_token = np.random.choice(next_tokens, p=probabilities)
         return next_token
 
     def gen_sentence(self, context, sentence_length):
@@ -45,6 +43,12 @@ class NGramModel:
                 sentences[s, i] = self.predict_next_token(context)
         return sentences
 
+    def _calculate_smoothed_prob(self, context, next_token):
+        # Calculate smoothed probability of next_token given context
+        prob = (self.ngrams[context][next_token] + self.smoothing_factor) / \
+            (sum(self.ngrams[context].values()) + (self.cfg.ns[-1] * self.smoothing_factor))
+        return prob
+
     def print_dict(self):
         # Printing the defaultdict
         for key_outer, inner_dict in self.ngrams.items():
@@ -52,7 +56,7 @@ class NGramModel:
             for key_inner, value in inner_dict.items():
                 print(f"Inner Key: {key_inner}, Value: {value}")
 
-    def compute_perplexity(self, test_set, smoothing_factor, vocab_size):
+    def compute_perplexity(self, test_set):
         total_log_prob = 0
         num_tokens = 0
 
@@ -61,11 +65,10 @@ class NGramModel:
 
             for i in range(self.n - 1, sentence.size(0)):
                 context = tuple(sentence[i - self.n + 1:i].tolist())
+                assert len(context) == self.n - 1
                 next_token = sentence[i].item()
                 # Compute the probability of the next token given the context
-                prob = (self.ngrams[context][next_token] + smoothing_factor) / \
-                    (sum(self.ngrams[context].values()) + (vocab_size * smoothing_factor))
-
+                prob = self._calculate_smoothed_prob(context, next_token)
                 # Update total log probability and number of tokens
                 total_log_prob += np.log(prob)
                 num_tokens += 1
