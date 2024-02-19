bfb983f45efba8c5c97b98029d23aa1a44551917
aboitreaud <aboitreaud@gmail.com>
2024-02-19 14:47:05 +0800

    Update ngram_training.py

--- ngram_training.py
+++ ngram_training.py
@@ -1,26 +1,26 @@
 import wandb
 import numpy as np
 
-from ngram_model import NGramModel
+from ngram_model import NGramModel, HierarchicalNGram
 from context_free_grammar import CFG
 
 wandb.login()
 
-max_epochs = 500
+max_epochs = 200
 batch_size = 5000
 eval_iters = 100
 
 # cfg = CFG(L=5, ns=[1, 3, 3, 3, 9, 10], nr=[2, 2, 2, 2, 2], T=[2, 2, 4, 4, 8])
-cfg = CFG(L=3, ns=[1, 10, 100, 1000], nr=[1, 1, 1], T=[8, 8, 8])
+cfg = CFG(L=2, ns=[1, 9, 10], nr=[2, 2], T=[8, 8])
 
-n = 3
+n = 9
 k_smoothing = 0.1
 m = NGramModel(n, k_smoothing, cfg)
 
 test_set = cfg.sample_flattened(eval_iters)[0]
 
 
-def train(model: NGramModel):
+def train_regular_ngram(model: NGramModel):
     running_acc_mean = []
     for epoch in range(max_epochs):
         sentences = cfg.sample_flattened(batch_size)[0]
@@ -56,6 +56,41 @@ def train(model: NGramModel):
     # log the mean accuracy over the last 50 epochs
     wandb.log({"Accuracy mean over 50 last epochs": np.mean(running_acc_mean[-50:])})
 
+
+def train_hierarchical_ngram(model: HierarchicalNGram):
+    running_acc_mean = []
+    for epoch in range(max_epochs):
+        sentences = cfg.sample_flattened(batch_size)[0]
+        for s in range(sentences.size(1)):
+            model.simple_ngrams(sentences[0, s, :])
+        gen_sentences = model.generate_sentence(eval_iters)
+        gen_sentences = gen_sentences.view([eval_iters] + cfg.T)
+        acc = cfg.frac_of_gramatically_correct_sentences(gen_sentences)
+        running_acc_mean.append(acc*100)
+
+        # compute per-level errors
+        # a sentence can only be good at level i if it was good at all levels beteewn L and i+1
+        correct_sentences = np.zeros(cfg.L)
+        for sentence in gen_sentences:
+            _, err = cfg.collapse_and_get_err(sentence)
+            for i in range(len(err) - 1, -1, -1):
+                if err[i].sum() != 0:
+                    break
+                else:
+                    correct_sentences[i] += 1
+        errors = np.array(correct_sentences) / eval_iters * 100
+
+        log_dict = {"nb sentences seen": (epoch + 1) * batch_size,
+                    "accuracy": acc * 100,
+                    }
+        for i, err in enumerate(errors):
+            log_dict[f'% of correct sentences at level {i}'] = err
+        wandb.log(log_dict)
+        print(f"Epoch [{epoch+1}/{max_epochs}] accuracry: {acc*100}%")
+    # log the mean accuracy over the last 50 epochs
+    wandb.log({"Accuracy mean over 50 last epochs": np.mean(running_acc_mean[-50:])})
+
+
 conf = {'model.n': m.n,
         'model.k_smoothing': k_smoothing,
         'cfg': {'L': cfg.L,
@@ -64,7 +99,7 @@ conf = {'model.n': m.n,
                 'T': cfg.T},
         'eval_iters': eval_iters,
         'batch_size': batch_size}
-wandb.init(project='CFG-ngram_first_try', name=f'{m.n}-gram', config=conf)
+wandb.init(project='CFG-ngram_first_try', name=f'{m.n}-gram {cfg.L} levels {cfg.nr} rules', config=conf)
 
 # wandb.watch(m, log='all')
 train(m)
